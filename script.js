6dbb47e5f19e138d55f1cba91cf47ed274d82d1c
Linda <lindashen3@gmail.com>
2022-01-06 01:40:38 -0500

    set up img endpoint url to input poster path

--- script.js
+++ script.js
@@ -7,27 +7,20 @@ movieApp.init = () => {
 }
 
 movieApp.key = '1a6e23d16ad2ac274c9fef47acd3b5ff';
-// Initialize preset data in the dedicated properties
-// - apiURL
-// - apiKey
-// - userQuery
 
 // form submit event listener 
 movieApp.formSubmit = () => {
-  // get form element - to attach event listener
-  const formElement = document.querySelector('form');
-  formElement.addEventListener('submit', function(event){
-    event.preventDefault(); 
-    const userQuery = event.target[0].value;
-    movieApp.getData(userQuery);
-  });
-
-  // get input string from user
-  
-  // movieApp.getData();
-    // pass string into query
+    // get form element - to attach event listener
+    const formElement = document.querySelector('form');
+    formElement.addEventListener('submit', function(event){
+        event.preventDefault(); 
+        const userQuery = event.target[0].value;
+        movieApp.getData(userQuery);
+    });
 }
 
+// GetData Function
+// - using apiURL, apiKey, userQuery
 movieApp.getData = (userQuery) => {
     const url = new URL('https://api.themoviedb.org/3/search/movie');
     url.search = new URLSearchParams({
@@ -42,36 +35,38 @@ movieApp.getData = (userQuery) => {
     });
 }
 
-// Methods > Search function
-// Create a method to update the variable (userQuery) based on user keyword input
 // Create a method to generate a random movie id based on random function
 
 movieApp.displayData = (jsonData) => {
-    console.log(jsonData);
-    // display data method
-    const randomIndex = Math.floor(Math.random() * jsonData.results.length);
-    // console.log(randomIndex);
-    // console.log(jsonData.results[randomIndex].title);
-    // console.log(jsonData.results[randomIndex].overview);
+  // display data method
+  const randomIndex = Math.floor(Math.random() * jsonData.results.length);
+  // get image from API endpoint
+  const imgEndpoint = `http://image.tmdb.org/t/p/w185${jsonData.results[randomIndex].poster_path}`;
 
-    // get main element from page 
-    const mainContainer = document.querySelector('main');
-    const mainSection = document.createElement('section');    
-    mainSection.classList.add('mainSection');
-    
-    const imageDiv = document.createElement('div');
-    const imageElement = document.createElement('img');
-    imageElement.src = jsonData.results[randomIndex].poster_path;
+  // get main element from page
+  const mainContainer = document.querySelector("main");
+  const mainSection = document.createElement("section");
+  mainSection.classList.add("mainSection");
 
-    // create div to hold description 
-    
-    // append results to page
-    mainContainer.appendChild(mainSection);
-    mainSection.appendChild(imageDiv);
-    imageDiv.appendChild(imageElement);
+  const imageDiv = document.createElement("div");
+  const imageElement = document.createElement("img");
+  imageElement.src = imgEndpoint;
+
+  // create div to hold description
+  const descripDiv = document.createElement("div");
+  const header = document.createElement("h2");
+  header.textContent = jsonData.results[randomIndex].title;
+
+  mainSection.appendChild(descripDiv);
+  descripDiv.appendChild(header);
+  
+  // append results to page
+  mainContainer.appendChild(mainSection);
+  mainSection.appendChild(imageDiv);
+  imageDiv.appendChild(imageElement);
 }
 
-// Use API endpoint/search/movie, apply the query parameter for closest match
+// error handling tasks
 // When the API call is successful, display the result by appending the data to the results div
 // If the API call fails, display an error message
 
